7c93095520e16c6ac35bbe8b996c57a3059e28fc
Marlon Lobo Furukawa <marlondossantosfurukawa@gmail.com>
2022-06-13 11:10:48 -0300

    Init Front

---
+++ ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
