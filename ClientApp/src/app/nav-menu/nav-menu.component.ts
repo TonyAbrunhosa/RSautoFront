0532a5dbf893f95907c89bedde1d3d6514bb3990
Marlon Lobo Furukawa <marlondossantosfurukawa@gmail.com>
2022-06-14 21:58:30 -0300

    Ajustes Layout e bootstrap

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,19 +1,45 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.css'],
+  animations: [
+    trigger('widthGrow', [
+      state('closed', style({
+        width: 0,
+      })),
+      state('open', style({
+        width: 400
+      })),
+      transition('* => *', animate(150))
+    ]),
+  ]
 })
 export class NavMenuComponent {
   isExpanded = false;
+  largeSidebar = true;
+  miniSidebar = false;
 
   collapse() {
-    this.isExpanded = false;
+    debugger;
+    this.isExpanded = true;
+    this.largeSidebar = false;
+    this.miniSidebar = true;
   }
 
-  toggle() {
+  openNav() {
+    debugger;
     this.isExpanded = !this.isExpanded;
+    this.largeSidebar = true;
+    this.miniSidebar = false;
+  }
+  state = "closed";
+
+  changeState(): void {
+    (this.state == "closed") ? this.state = "open" : this.state = "closed";
+
   }
 }
