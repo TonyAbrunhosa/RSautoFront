277358ad76c333ac4897cf1a9cac3aafd7882746
Joao Victor Fernandes de Souza Silva <joao.vfsilva@localiza.com>
2023-05-03 11:30:50 -0300

    feat: added changes

--- ClientApp/src/pages/VehiclesStore/index.js
+++ ClientApp/src/pages/VehiclesStore/index.js
@@ -15,7 +15,6 @@ const VehiclesStore = () => {
   const [brands, setBrands] = useState([]);
   const [models, setModels] = useState([]);
   const [customers, setCustomers] = useState([]);
-  const [fetching, setFetching] = useState(false);
   const [initialValues, setInitialValues] = useState({});
 
   const onRequiredTypeChange = ({ requiredMarkValue }) =>
@@ -40,7 +39,6 @@ const VehiclesStore = () => {
     setModels([]);
     setCustomers(['João Victor - 67.117.218/0001-00']);
     setInitialValues({});
-    setFetching(false);
   }, []);
 
   return (
@@ -76,27 +74,8 @@ const VehiclesStore = () => {
         >
           <FormRow>
             <Form.Item
-              name="description"
-              label="Descrição"
-              validateTrigger={['onChange', 'onBlur']}
-              rules={[
-                {
-                  required: true,
-                  whitespace: true,
-                  message: 'A descrição do veículo é obrigatória',
-                },
-              ]}
-              tooltip="Descrição do veículo"
-              style={{ width: '100%' }}
-            >
-              <Input placeholder="Digite a descrição do veículo..." />{' '}
-            </Form.Item>
-          </FormRow>
-
-          <FormRow>
-            <Form.Item
               name="brand"
-              style={{ width: '33%' }}
+              style={{ width: '49%' }}
               label="Marca"
               validateTrigger={['onChange']}
               rules={[
@@ -113,7 +92,7 @@ const VehiclesStore = () => {
 
             <Form.Item
               name="model"
-              style={{ width: '33%' }}
+              style={{ width: '49%' }}
               label="Modelo"
               validateTrigger={['onChange']}
               rules={[
@@ -127,22 +106,40 @@ const VehiclesStore = () => {
             >
               <Select mode="tags" options={models} />
             </Form.Item>
-
+          </FormRow>
+          <FormRow>
             <Form.Item
-              name="anoModelo"
-              style={{ width: '33%' }}
-              label="Ano/Modelo"
+              name="tipoCombustivel"
+              style={{ width: '49%' }}
+              label="Tipo do combustível"
               validateTrigger={['onChange']}
               rules={[
                 {
                   required: true,
                   whitespace: true,
-                  message: 'O ano/modelo do veículo é obrigatório',
+                  message: 'Tipo do combustível',
                 },
               ]}
-              tooltip="Ano/Modelo do veículo"
+              tooltip="Tipo do combustível do veículo"
             >
               <Select mode="tags" options={[]} />
+            </Form.Item>
+
+            <Form.Item
+              name="cilindrada"
+              label="Cilindrada"
+              validateTrigger={['onChange', 'onBlur']}
+              rules={[
+                {
+                  required: true,
+                  whitespace: true,
+                  message: 'A cilindrada do veículo é obrigatória',
+                },
+              ]}
+              tooltip="Cilindrada do veículo"
+              style={{ width: '49%' }}
+            >
+              <Input placeholder="Digite a cilindrada do veículo..." />{' '}
             </Form.Item>
           </FormRow>
 
@@ -159,7 +156,7 @@ const VehiclesStore = () => {
                 },
               ]}
               tooltip="O número da placa do veículo"
-              style={{ width: '33%' }}
+              style={{ width: '49%' }}
             >
               <Input placeholder="Digite o número da placa..." />{' '}
             </Form.Item>
@@ -175,31 +172,15 @@ const VehiclesStore = () => {
                 },
               ]}
               tooltip="Quilometragem do veículo"
-              style={{ width: '33%' }}
+              style={{ width: '49%' }}
             >
               <Input placeholder="Digite a quilometragem do veículo..." />{' '}
-            </Form.Item>
-            <Form.Item
-              name="cilindrada"
-              label="Cilindrada"
-              validateTrigger={['onChange', 'onBlur']}
-              rules={[
-                {
-                  required: true,
-                  whitespace: true,
-                  message: 'A cilindrada do veículo é obrigatória',
-                },
-              ]}
-              tooltip="Cilindrada do veículo"
-              style={{ width: '33%' }}
-            >
-              <Input placeholder="Digite a cilindrada do veículo..." />{' '}
             </Form.Item>
           </FormRow>
           <FormRow>
             <Form.Item
-              name="Cliente"
-              label="cliente"
+              name="cliente"
+              label="Cliente"
               validateTrigger={['onChange', 'onBlur']}
               rules={[
                 {
